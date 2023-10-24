45716630ab022f178759f4395bad7d42c79920a7
KaylaSyahma <ksyahma22@gmail.com>
2023-10-24 08:47:46 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let menuIcon = document.querySelector('#menu-icon')
+// let navbar = document.querySelector('.navbar')
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x')
+//     navbar.classList.toggle('active')
+// }
+
+// window.onscroll = () => {
+//     menuIcon.classList.remove('remove')
+//     navbar.classList.remove('active')
+// }
+
